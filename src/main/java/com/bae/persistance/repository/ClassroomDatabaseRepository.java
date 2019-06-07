b92ae360da7659e2982a6418e28191ace11665ca
JackFlanagan93 <JackFlanagan93@outlook.com>
2019-06-07 11:04:07 +0100

    persistance.repository & persistance.domain complete, build success and deployment

--- src/main/java/com/bae/persistance/repository/ClassroomDatabaseRepository.java
+++ src/main/java/com/bae/persistance/repository/ClassroomDatabaseRepository.java
@@ -1,10 +1,25 @@
 package com.bae.persistance.repository;
 
-public class Classroom implements Classroom_Interface{
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import com.bae.util.JSONUtil;
+
+@Default
+public class ClassroomDatabaseRepository implements Classroom_Interface{
+	
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+	
+	@Inject
+	private JSONUtil util;
+	
+	
 	public String getAllClassrooms() {
 		// TODO Auto-generated method stub
-		return null;
+		return "getAllClassrooms";
 	}
 
 	public String findClassroom(String classroomID) {
--- src/main/java/com/bae/persistance/repository/TraineesDatabaseRepository.java
+++ src/main/java/com/bae/persistance/repository/TraineesDatabaseRepository.java
@@ -1,10 +1,25 @@
 package com.bae.persistance.repository;
 
-public class Trainees implements Trainees_Interface {
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import com.bae.util.JSONUtil;
+
+@Default
+public class TraineesDatabaseRepository implements Trainees_Interface {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+	
+	@Inject
+	private JSONUtil util;
+	
+	
 	public String getAllTrainees() {
 		// TODO Auto-generated method stub
-		return null;
+		return "getAllTrainees";
 	}
 
 	public String findTrainee(String traineeID) {
